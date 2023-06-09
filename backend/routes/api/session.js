ede06493497c964c159889bf7313b622fde3424c
Jennie Chen <jennie199911@gmail.com>
2023-06-08 22:41:34 -0700

    Add User input validation on user signup backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -43,10 +43,8 @@ router.post('/',validateLogin,async(req,res,next)=>{
     }
     const safeUser = {
         id:user.id,
-            firstName:user.firstName,
-            lastName:user.lastName,
-            email:user.email,
-            username:user.username,
+        email:user.email,
+        username:user.username,
     };
 
     await setTokenCookie(res,safeUser);
